b92511dd59b49294f8174976d6f00cf7e1744a4c
marorcas <ortzmc@gmail.com>
2024-08-08 23:35:27 +1000

    Print basic game instructions to console

--- minesweeper/Main.java
+++ minesweeper/Main.java
@@ -6,7 +6,18 @@ public class Main {
         Grid grid = new Grid(10, 10, 10);
 
         System.out.println();
-        System.out.println("  =============== Play Minesweeper! ===============  ");
+        System.out.println("   =============== Play Minesweeper! ===============   ");
+        System.out.println();
+
+        System.out.println("   How to play:");
+        System.out.println("   1. Enter a column number");
+        System.out.println("   2. Enter a row number");
+        System.out.println(
+                "   3. If a number appears in the box of your chosen coordinates, this indicates how many mines surround that box");
+        System.out.println("   4. If you click on a mine, it's game over!");
+        System.out.println();
+        System.out.println("   Have fun :)");
+        System.out.println();
 
         grid.getGrid();
     }
