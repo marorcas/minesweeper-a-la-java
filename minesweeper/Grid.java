33a9b4f78a35afdc2bff4b26f502aa82aede26bc
marorcas <ortzmc@gmail.com>
2024-08-08 23:27:49 +1000

    Print coordinates to console; add simple welcome message

--- minesweeper/Grid.java
+++ minesweeper/Grid.java
@@ -6,42 +6,65 @@ public class Grid {
     private int cols;
     private int rows;
     private int minecount;
-    private int[][] minesArray;
+    private Character[][] minesArray;
+    private Character[][] gameArray;
 
     public Grid(int cols, int rows, int minecount) {
         this.cols = cols;
         this.rows = rows;
         this.minecount = minecount;
-        this.minesArray = new int[cols][rows];
-        this.getBombs();
+        this.minesArray = new Character[cols][rows];
+        this.populateMinesArray();
+        this.gameArray = new Character[cols][rows];
+        this.populateGameArray();
     }
 
-    public void getBombs() {
+    public void populateMinesArray() {
         // fill 2-D array with zeros
         for (int i = 0; i < cols; i++) {
-            Arrays.fill(this.minesArray[i], 0);
+            Arrays.fill(this.minesArray[i], '-');
         }
 
         // generate random indexes for bomb placements
         // have to make sure generated placements are not repeated
+        // 'x' represents a bomb
         int count = 0;
         while (count < this.minecount) {
             int randomCol = (int) (Math.random() * (cols));
             int randomRow = (int) (Math.random() * (rows));
-            if (this.minesArray[randomCol][randomRow] != 1) {
-                this.minesArray[randomCol][randomRow] = 1;
+            if (this.minesArray[randomCol][randomRow] != 'x') {
+                this.minesArray[randomCol][randomRow] = 'x';
                 count++;
             }
         }
     }
 
+    public void populateGameArray() {
+        for (int i = 0; i < cols; i++) {
+            Arrays.fill(this.gameArray[i], ' ');
+        }
+    }
+
     public void getGrid() {
         System.out.println();
+
+        for (int i = 0; i < this.cols; i++) {
+            if (i == 0) {
+                System.out.print("   ");
+            }
+            System.out.print("  " + i + "  ");
+        }
+
+        System.out.println();
+
         for (int i = 0; i < this.cols; i++) {
             for (int j = 0; j < this.rows; j++) {
-                System.out.print("[" + this.minesArray[i][j] + "]");
+                if (j == 0) {
+                    System.out.print(" " + i + " ");
+                }
+                System.out.print("[ " + this.gameArray[i][j] + " ]");
             }
-            System.out.println();
+            System.out.println("\n");
         }
     }
 }
--- minesweeper/Main.java
+++ minesweeper/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Grid grid = new Grid(10, 10, 10);
 
+        System.out.println();
+        System.out.println("  =============== Play Minesweeper! ===============  ");
+
         grid.getGrid();
     }
 }
