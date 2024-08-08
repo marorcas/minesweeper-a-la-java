66c728e63716ec90ed60cfb4147aa4d8cf34e30a
marorcas <ortzmc@gmail.com>
2024-08-08 20:45:14 +1000

    Add basic idea for randomised bomb placement

--- minesweeper/Grid.java
+++ minesweeper/Grid.java
@@ -1,20 +1,47 @@
 package minesweeper;
 
+import java.util.Arrays;
+
 public class Grid {
     private int cols;
     private int rows;
+    private int minecount;
+    private int[][] minesArray;
 
-    public Grid(int cols, int rows) {
+    public Grid(int cols, int rows, int minecount) {
         this.cols = cols;
         this.rows = rows;
+        this.minecount = minecount;
+        this.minesArray = new int[cols][rows];
+        this.getBombs();
+    }
+
+    public void getBombs() {
+        // fill 2-D array with zeros
+        for (int i = 0; i < cols; i++) {
+            Arrays.fill(this.minesArray[i], 0);
+        }
+
+        // generate random indexes for bomb placements
+        // have to make sure generated placements are not repeated
+        int count = 0;
+        while (count < this.minecount) {
+            int randomCol = (int) (Math.random() * (cols));
+            int randomRow = (int) (Math.random() * (rows));
+            if (this.minesArray[randomCol][randomRow] != 1) {
+                this.minesArray[randomCol][randomRow] = 1;
+                count++;
+            }
+        }
     }
 
     public void getGrid() {
-        for (int i = 0; i < this.rows; i++) {
-            for (int j = 0; j < this.cols; j++) {
-                System.out.print("[ ]");
+        System.out.println();
+        for (int i = 0; i < this.cols; i++) {
+            for (int j = 0; j < this.rows; j++) {
+                System.out.print("[" + this.minesArray[i][j] + "]");
             }
-            System.out.println("\n");
+            System.out.println();
         }
     }
 }
--- minesweeper/Main.java
+++ minesweeper/Main.java
@@ -3,7 +3,7 @@ package minesweeper;
 public class Main {
 
     public static void main(String[] args) {
-        Grid grid = new Grid(10, 10);
+        Grid grid = new Grid(10, 10, 10);
 
         grid.getGrid();
     }
